33dd5ddb55536ddd29eca8fbce40832c91ca39dd
jitwxs <jitwxs@foxmail.com>
2018-11-07 12:40:57 +0800

    更新CorsFilter

--- paas/src/main/java/jit/edu/paas/Application.java
+++ paas/src/main/java/jit/edu/paas/Application.java
@@ -1,7 +1,11 @@
 package jit.edu.paas;
 
+import jit.edu.paas.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -13,5 +17,16 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean registerFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
+        bean.addUrlPatterns("/*");
+        bean.setFilter(new CorsFilter());
+        // 过滤顺序，从小到大依次过滤
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return bean;
     }
 }
---
+++ paas/src/main/java/jit/edu/paas/filter/CorsFilter.java
@@ -0,0 +1,63 @@
+package jit.edu.paas.filter;
+
+import jit.edu.paas.commons.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 跨域过滤器
+ * @author jitwxs
+ * @since 2018/10/16 20:53
+ */
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        // 不使用*，自动适配跨域域名，避免携带Cookie时失效
+        String origin = request.getHeader("Origin");
+        if(StringUtils.isNotBlank(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
+        // 自适应所有自定义头
+        String headers = request.getHeader("Access-Control-Request-Headers");
+        if(StringUtils.isNotBlank(headers)) {
+            response.setHeader("Access-Control-Allow-Headers", headers);
+            response.setHeader("Access-Control-Expose-Headers", headers);
+        }
+
+        // 允许跨域的请求方法类型
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        // 预检命令（OPTIONS）缓存时间，单位：秒
+        response.setHeader("Access-Control-Max-Age", "3600");
+        // 明确许可客户端发送Cookie，不允许删除字段即可
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    /*
+    @Bean
+    public FilterRegistrationBean registerFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
+        bean.addUrlPatterns("/*");
+        bean.setFilter(new CorsFilter());
+        // 过滤顺序，从小到大依次过滤
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return bean;
+    }
+     */
+}
